cf4368d99e6abc13547fe2b7976527fbb35943d1
Chris <53177842+ChristopherMarais@users.noreply.github.com>
2024-02-05 14:17:43 -0500

    Create deploy.R
    ShinyApps.io deployment script

---
+++ 05_DASHBOARD/deploy.R
@@ -0,0 +1,7 @@
+# Authenticate
+setAccountInfo(name = Sys.getenv("SHINY_ACC_NAME"),
+               token = Sys.getenv("TOKEN"),
+               secret = Sys.getenv("SECRET"))
+
+# Deploy
+deployApp(appFiles = c("app.R", "likes.rds"))
