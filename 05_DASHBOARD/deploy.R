2d27f283b4ed61af02e34d5516ef4d26436dbd74
Chris <53177842+ChristopherMarais@users.noreply.github.com>
2024-02-06 14:56:18 -0500

    update deploy script

--- 05_DASHBOARD/deploy.R
+++ 05_DASHBOARD/deploy.R
@@ -1,11 +1,7 @@
 # Authenticate
-# setAccountInfo(name = Sys.getenv("SHINY_ACC_NAME"),
-#                token = Sys.getenv("TOKEN"),
-#                secret = Sys.getenv("SECRET"))
-
-setAccountInfo(name = Sys.getenv("christophermarais"),
-               token = Sys.getenv("908D29B518B4A47C935C6E63B03D4C73"),
-               secret = Sys.getenv("cH2hwZOTvRMKm5ouNDaX6zJrDo0WVVVsvOTzKRqu"))
+setAccountInfo(name = Sys.getenv("SHINY_ACC_NAME"),
+               token = Sys.getenv("TOKEN"),
+               secret = Sys.getenv("SECRET"))
 
 # Deploy
-deployApp(appFiles = c("app.R", "likes.rds"))
+deployApp(appFiles = c("app.R"))
