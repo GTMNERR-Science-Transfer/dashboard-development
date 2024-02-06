a116e39689ea51614d15ba951d305a298d73280c
Chris <53177842+ChristopherMarais@users.noreply.github.com>
2024-02-06 15:03:33 -0500

    load library

--- 05_DASHBOARD/deploy.R
+++ 05_DASHBOARD/deploy.R
@@ -1,7 +1,11 @@
+# Load the rsconnect package
+library(rsconnect)
+
 # Authenticate
-setAccountInfo(name = Sys.getenv("SHINY_ACC_NAME"),
-               token = Sys.getenv("TOKEN"),
-               secret = Sys.getenv("SECRET"))
+rsconnect::setAccountInfo(
+    name = Sys.getenv("SHINY_ACC_NAME"),
+    token = Sys.getenv("TOKEN"),
+    secret = Sys.getenv("SECRET"))
 
 # Deploy
 deployApp(appFiles = c("app.R"))
