880af6c5d287a6ee70efead1595d587ba8d106ca
Geraldine Klarenberg <gklarenberg@ufl.edu>
2024-10-14 13:15:14 +0200

    Date picker shows up now and works - BUT plot only updates if you select a station (because that's in observeEvent). Have to change latter so it also updates with date change. And possibly update so it shows available dates for a selected variable? Use req()? Added header that water_quality2 is old.

--- modules/waterquality.R
+++ modules/waterquality.R
@@ -34,7 +34,7 @@ WQ_data_units = WQ_df %>%
 
 #### Functions ####
 # make dataframe for click plots - filter for correct stations
-filter_dataframe <- function(df, filter_value = NULL) {
+filter_dataframe <- function(df, filter_value = NULL, date_range = NULL) {
   if (!is.null(filter_value)) {
     # Step 1: Identify the relevant RowIDs
     relevant_row_ids <- df %>%
@@ -61,16 +61,21 @@ filter_dataframe <- function(df, filter_value = NULL) {
     pivot_wider(names_from = ComponentLong, 
                 values_from = Result,
                 values_fn = list(Result = ~ mean(as.numeric(.), na.rm = TRUE))) %>%
-    mutate(SampleDate = ymd_hms(SampleDate)) %>% # 
-    #mutate(SampleDate = str_extract(SampleDate, "[0-9]{4}-[0-9]{2}-[0-9]{2}")) %>% 
-    #mutate(SampleDate = ymd(SampleDate)) %>% # these two lines are another option to only get ymd
-    mutate(across(-c(SampleDate, geometry, StationCode, site_friendly), ~ as.numeric(.))) %>%
-    mutate(SampleDate = as.Date(SampleDate)) %>%
+    #mutate(SampleDate = ymd_hms(SampleDate)) %>% # 
+    mutate(SampleDate = str_extract(SampleDate, "[0-9]{4}-[0-9]{2}-[0-9]{2}")) %>% 
+    mutate(SampleDate = ymd(SampleDate)) %>% # these two lines are another option to only get ymd,
+    # needed to use this since some datasets only have ymd (no hms) so that makes ymd_hms fail
+    #mutate(across(-c(SampleDate, geometry, StationCode, site_friendly), ~ as.numeric(.))) %>%
+    #mutate(SampleDate = as.Date(SampleDate)) %>%
     group_by(SampleDate, geometry, StationCode, site_friendly) %>%
     summarize(across(everything(), ~mean(.x, na.rm = TRUE))) #%>% # across(everything()) is not necessary,
   # strictly speaking, but it's nice to keep for if we ever want to adjust this function
   # to work for more than 1 variable
     #select(where(~ n_distinct(.) > 2))
+  
+  if(!is.null(date_range)){
+    wide_df <- filter(wide_df, between(SampleDate, date_range[1], date_range[2]))
+  }
   
   return(wide_df)
 }
@@ -89,9 +94,29 @@ create_dropdown <- function(df, ns) {
   )
 }
 
+# New function to create the date picker
+create_date <- function(df, ns) {
+  # Get the column names except the dates and column names and geometry
+  date_column <- df %>% 
+    pull(SampleDate)
+  print(paste("Creating date range"))
+  
+  dateRangeInput( # This creates a date vector of length 2
+    inputId = ns("date_range"),
+    label = "Select a Date Range",
+    start = NULL, 
+    end = NULL, # will use current date
+    min = min(date_column),
+    max = max(date_column),
+    format = "mm/dd/yyyy",
+    separator = " - "
+  )
+}
+
 # Modified create_plot function
-create_plot <- function(df, units_df, loc_name = NULL, selected_column) {
+create_plot <- function(df, units_df, loc_name = NULL, selected_column, date_range) {
   print(paste("Creating plot for", selected_column, "at", loc_name))
+  # At this point, the datafrme has already been filtered for the correct station
   # Initialize the plot with the x-axis
   fig <- plot_ly(df, x = ~SampleDate)
   
@@ -106,7 +131,7 @@ create_plot <- function(df, units_df, loc_name = NULL, selected_column) {
   #   selected_column <- column_names[1]
   # }
   
-  # Loop through each column and add a trace
+  # Loop through each column and add a trace -> Check if this is the best way (maybe rather filter df?)
   for (i in seq_along(column_names)) {
     fig <- fig %>%
       add_trace(y = df[[column_names[i]]], name = column_names[i], type = 'scatter', mode = 'lines+markers',
@@ -155,8 +180,10 @@ WINPageUI <- function(id) {
       )
     ),
     fluidRow(
-      column(width = 12, uiOutput(ns("dropdown_ui")), style = "position:relative;z-index:10000;")
+      column(width = 6, uiOutput(ns("dropdown_ui")), style = "position:relative;z-index:10000;"),
       # style is to make sure the dropdown menu shows over the map zoom tools
+      column(width = 6, uiOutput(ns("date_range")), style = "position:relative;z-index:10000;")
+      # style is to make sure the date selection tool shows over the map zoom tools
     ),
     fluidRow(
       column(width = 8, 
@@ -254,8 +281,15 @@ WINPageServer <- function(id, parentSession) {
     #### Create the dropdown UI #####
     output$dropdown_ui <- renderUI({
       print("Rendering dropdown UI")
-      df <- filter_dataframe(WQ_df)
+      df <- filter_dataframe(WQ_df) # we will have to make this reactive??
       create_dropdown(df, ns)
+    })
+    
+    #### Create the date input UI #####
+    output$date_range <- renderUI({
+      print("Picking date range")
+      df <- filter_dataframe(WQ_df) # we will have to make this reactive??
+      create_date(df, ns)
     })
     
     #### Observe marker clicks and update plot when station changes ####
@@ -302,7 +336,7 @@ WINPageServer <- function(id, parentSession) {
         # Store the StationCode in a reactive value
         clicked_station(clicked_data$StationCode)
         
-        df <- filter_dataframe(WQ_df, filter_value = clicked_station()) 
+        df <- filter_dataframe(WQ_df, filter_value = clicked_station(), date_range = selected_date_range()) 
         
         output$plot <- renderPlotly({
           if (selected_column() %in% names(df)) { # only plot if variable exists for that station
@@ -349,19 +383,25 @@ WINPageServer <- function(id, parentSession) {
       
     })
     
-    #### Update plot when dropdown selection changes ####
+    #### Update plot when dropdown selection and/or date changes ####
     # Reactive value for the selected column
     selected_column <- reactive({
       print(paste("Selected column changed to:", input$column_selector))
       input$column_selector
     })
     
+    selected_date_range <- reactive({
+      print(paste("Selected date range:", input$date_range))
+      input$date_range
+    })
+    
     observeEvent(selected_column(), {
       req(clicked_station())  # Ensure station is selected
+      #req(date_range())
       
-      print(paste("Updating plot for", selected_column(), "at station", clicked_station()))
+      print(paste("Updating plot for", selected_column(), "at station", clicked_station(), "for", date_range[1], " to ", date_range[2]))
       
-      df <- filter_dataframe(WQ_df, filter_value = clicked_station())
+      df <- filter_dataframe(WQ_df, filter_value = clicked_station(), date_range = selected_date_range())
       
       output$plot <- renderPlotly({
         if (selected_column() %in% names(df)) { # only plot if variable exists for that station
