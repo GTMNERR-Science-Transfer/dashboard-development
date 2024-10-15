50fedc27c6451b6d2e58db3dafee0347525260dd
Geraldine Klarenberg <gklarenberg@ufl.edu>
2024-10-14 21:16:40 -0400

    Moved functions from the waterquality module to the functions script. Added comments to wq module on things that need to be checked

--- functions.R
+++ functions.R
@@ -1,4 +1,6 @@
-# Function to find the directory of a file named app.R
+#### FUNCTIONS ####
+
+##### Find the directory of a file named app.R #####
 find_directory_of_file <- function(file_name, start_dir=getwd()) {
   # Recursively list all files starting from the start_dir
   app_dir <- fs::dir_ls(start_dir, recurse = TRUE, glob=file_name)
@@ -11,4 +13,135 @@ find_directory_of_file <- function(file_name, start_dir=getwd()) {
   } else {
     return(NULL) # Return NULL if the file is not found
   }
+}
+
+##### Filter df for station (click on plot) and date range, and make wide #####
+# make dataframe for click plots - filter for correct stations
+# and the date picker: so this takes 2 reactive values, stationCode and
+# selected_date_range
+filter_dataframe <- function(df, filter_value = NULL, date_range = NULL) {
+  if (!is.null(filter_value)) {
+    # Step 1: Identify the relevant RowIDs
+    relevant_row_ids <- df %>%
+      filter(value == filter_value) %>%
+      pull(RowID)
+    
+    # Step 2: Filter the entire dataframe to keep only rows with the relevant RowIDs
+    filtered_df <- df %>%
+      filter(RowID %in% relevant_row_ids)
+  } else {
+    # If no filter_value is provided, skip the filtering step
+    filtered_df <- df
+  }
+  
+  # Step 3: Create wide dataframe
+  wide_df <- filtered_df %>%
+    pivot_wider(names_from = variable, values_from = value, values_fn = first) %>%
+    filter(!is.na(ComponentLong)) %>% # stop gap measure because there are NAs from
+    # replacing ComponentLong names in the cleaning script
+    select(SampleDate, # we could also make these arguments for the function?
+           ComponentLong, 
+           Result,
+           geometry, StationCode, site_friendly) %>%
+    pivot_wider(names_from = ComponentLong, 
+                values_from = Result,
+                values_fn = list(Result = ~ mean(as.numeric(.), na.rm = TRUE))) %>%
+    #mutate(SampleDate = ymd_hms(SampleDate)) %>% # 
+    mutate(SampleDate = str_extract(SampleDate, "[0-9]{4}-[0-9]{2}-[0-9]{2}")) %>% 
+    mutate(SampleDate = ymd(SampleDate)) %>% # these two lines are another option to only get ymd,
+    # needed to use this since some datasets only have ymd (no hms) so that makes ymd_hms fail
+    #mutate(across(-c(SampleDate, geometry, StationCode, site_friendly), ~ as.numeric(.))) %>%
+    #mutate(SampleDate = as.Date(SampleDate)) %>%
+    group_by(SampleDate, geometry, StationCode, site_friendly) %>%
+    summarize(across(everything(), ~mean(.x, na.rm = TRUE))) #%>% # across(everything()) is not necessary,
+  # strictly speaking, but it's nice to keep for if we ever want to adjust this function
+  # to work for more than 1 variable
+  #select(where(~ n_distinct(.) > 2))
+  
+  if(!is.null(date_range)){
+    wide_df <- filter(wide_df, between(SampleDate, date_range[1], date_range[2]))
+  }
+  
+  return(wide_df)
+}
+
+##### Create dropdown with variables to plot #####
+create_dropdown <- function(df, ns) {
+  # Get the column names except the dates and column names and geometry
+  column_names <- sort(colnames(df)[!colnames(df) %in% c("SampleDate", "geometry", "StationCode", "site_friendly")])
+  print(paste("Creating dropdown with choices:", paste(column_names, collapse=", ")))
+  
+  selectInput(
+    inputId = ns("column_selector"),
+    label = "Select a Variable of Interest",
+    choices = column_names,
+    selected = column_names[1]
+  )
+}
+
+##### Create the date picker #####
+create_date <- function(df, ns) {
+  # Get the column names except the dates and column names and geometry
+  date_column <- df %>% 
+    pull(SampleDate)
+  print(paste("Creating date range"))
+  
+  dateRangeInput( # This creates a date vector of length 2
+    inputId = ns("date_range"),
+    label = "Select a Date Range",
+    start = NULL, 
+    end = NULL, # will use current date
+    min = min(date_column),
+    max = max(date_column),
+    format = "mm/dd/yyyy",
+    separator = " - "
+  )
+}
+
+##### Create plot #####
+# Modified create_plot function: takes 1 reactive value: the variable (selected
+# column) 
+create_plot <- function(df, units_df, loc_name = NULL, selected_column, date_range) {
+  print(paste("Creating plot for", selected_column, "at", loc_name))
+  # At this point, the datafrme has already been filtered for the correct station
+  # Initialize the plot with the x-axis
+  fig <- plot_ly(df, x = ~SampleDate)
+  
+  # Get the column names except the dates and column names and geometry
+  column_names <- sort(colnames(df)[!colnames(df) %in% c("SampleDate", "geometry", "StationCode", "site_friendly")])
+  
+  # Create a named vector for Y-axis titles
+  y_axis_titles <- setNames(units_df$Unit, units_df$ComponentLong)
+  
+  # Ensure selected_column is not NULL or empty
+  # if (is.null(selected_column) || selected_column == "") {
+  #   selected_column <- column_names[1]
+  # }
+  
+  # Loop through each column and add a trace -> Check if this is the best way (maybe rather filter df?)
+  for (i in seq_along(column_names)) {
+    fig <- fig %>%
+      add_trace(y = df[[column_names[i]]], name = column_names[i], type = 'scatter', mode = 'lines+markers',
+                showlegend = FALSE,
+                visible = if (column_names[i] == selected_column) TRUE else FALSE)
+  }
+  
+  station_name <- unique(df$StationCode)
+  
+  # Customize the layout
+  fig <- fig %>%
+    layout(xaxis = list(title = 'Date'),
+           yaxis = list(title = y_axis_titles[selected_column]),
+           title = list(text = paste0("Mean daily ", selected_column, "<br>for ", loc_name, " (station code: ", station_name, ")"), 
+                        y = 0.90), 
+           margin = list(t = 60),
+           plot_bgcolor = '#e5ecf6',
+           xaxis = list(zerolinecolor = 'black',
+                        zerolinewidth = 2,
+                        gridcolor = 'azure1'),
+           yaxis = list(zerolinecolor = 'black',
+                        zerolinewidth = 2,
+                        gridcolor = 'azure1'))
+  
+  return(fig)
 }
--- modules/waterquality.R
+++ modules/waterquality.R
@@ -16,149 +16,7 @@ WQ_df <- readRDS("./03_Data_for_app/WQ_all.Rds")
 # Location data
 WQ_data_locations <- readRDS("./03_Data_for_app/WQ_data_locations.Rds")
 
-#### Process data further ####
-# Also move this to cleaning script
-WQ_data_units = WQ_df %>%
-  filter(variable %in% c("ComponentLong", # some will be duplicated because they differ between the 2 sets
-                         "Unit", "data_source")
-  ) %>%
-  #select(c(RowID, variable, value)) %>%
-  distinct(RowID, variable, value) %>% # strictly speaking also not necessary
-  pivot_wider(
-    names_from = variable,
-    values_from = value,
-    values_fill = list(value = NA),
-    values_fn = list(value = ~ first(.))
-  ) %>%
-  distinct(ComponentLong, Unit, data_source)
-
-#### Functions ####
-# make dataframe for click plots - filter for correct stations
-filter_dataframe <- function(df, filter_value = NULL, date_range = NULL) {
-  if (!is.null(filter_value)) {
-    # Step 1: Identify the relevant RowIDs
-    relevant_row_ids <- df %>%
-      filter(value == filter_value) %>%
-      pull(RowID)
-    
-    # Step 2: Filter the entire dataframe to keep only rows with the relevant RowIDs
-    filtered_df <- df %>%
-      filter(RowID %in% relevant_row_ids)
-  } else {
-    # If no filter_value is provided, skip the filtering step
-    filtered_df <- df
-  }
-  
-  # Step 3: Create wide dataframe
-  wide_df <- filtered_df %>%
-    pivot_wider(names_from = variable, values_from = value, values_fn = first) %>%
-    filter(!is.na(ComponentLong)) %>% # stop gap measure because there are NAs from
-    # replacing ComponentLong names in the cleaning script
-    select(SampleDate, # we could also make these arguments for the function?
-           ComponentLong, 
-           Result,
-           geometry, StationCode, site_friendly) %>%
-    pivot_wider(names_from = ComponentLong, 
-                values_from = Result,
-                values_fn = list(Result = ~ mean(as.numeric(.), na.rm = TRUE))) %>%
-    #mutate(SampleDate = ymd_hms(SampleDate)) %>% # 
-    mutate(SampleDate = str_extract(SampleDate, "[0-9]{4}-[0-9]{2}-[0-9]{2}")) %>% 
-    mutate(SampleDate = ymd(SampleDate)) %>% # these two lines are another option to only get ymd,
-    # needed to use this since some datasets only have ymd (no hms) so that makes ymd_hms fail
-    #mutate(across(-c(SampleDate, geometry, StationCode, site_friendly), ~ as.numeric(.))) %>%
-    #mutate(SampleDate = as.Date(SampleDate)) %>%
-    group_by(SampleDate, geometry, StationCode, site_friendly) %>%
-    summarize(across(everything(), ~mean(.x, na.rm = TRUE))) #%>% # across(everything()) is not necessary,
-  # strictly speaking, but it's nice to keep for if we ever want to adjust this function
-  # to work for more than 1 variable
-    #select(where(~ n_distinct(.) > 2))
-  
-  if(!is.null(date_range)){
-    wide_df <- filter(wide_df, between(SampleDate, date_range[1], date_range[2]))
-  }
-  
-  return(wide_df)
-}
-
-# New function to create the dropdown
-create_dropdown <- function(df, ns) {
-  # Get the column names except the dates and column names and geometry
-  column_names <- sort(colnames(df)[!colnames(df) %in% c("SampleDate", "geometry", "StationCode", "site_friendly")])
-  print(paste("Creating dropdown with choices:", paste(column_names, collapse=", ")))
-  
-  selectInput(
-    inputId = ns("column_selector"),
-    label = "Select a Variable of Interest",
-    choices = column_names,
-    selected = column_names[1]
-  )
-}
-
-# New function to create the date picker
-create_date <- function(df, ns) {
-  # Get the column names except the dates and column names and geometry
-  date_column <- df %>% 
-    pull(SampleDate)
-  print(paste("Creating date range"))
-  
-  dateRangeInput( # This creates a date vector of length 2
-    inputId = ns("date_range"),
-    label = "Select a Date Range",
-    start = NULL, 
-    end = NULL, # will use current date
-    min = min(date_column),
-    max = max(date_column),
-    format = "mm/dd/yyyy",
-    separator = " - "
-  )
-}
-
-# Modified create_plot function
-create_plot <- function(df, units_df, loc_name = NULL, selected_column, date_range) {
-  print(paste("Creating plot for", selected_column, "at", loc_name))
-  # At this point, the datafrme has already been filtered for the correct station
-  # Initialize the plot with the x-axis
-  fig <- plot_ly(df, x = ~SampleDate)
-  
-  # Get the column names except the dates and column names and geometry
-  column_names <- sort(colnames(df)[!colnames(df) %in% c("SampleDate", "geometry", "StationCode", "site_friendly")])
-  
-  # Create a named vector for Y-axis titles
-  y_axis_titles <- setNames(units_df$Unit, units_df$ComponentLong)
-  
-  # Ensure selected_column is not NULL or empty
-  # if (is.null(selected_column) || selected_column == "") {
-  #   selected_column <- column_names[1]
-  # }
-  
-  # Loop through each column and add a trace -> Check if this is the best way (maybe rather filter df?)
-  for (i in seq_along(column_names)) {
-    fig <- fig %>%
-      add_trace(y = df[[column_names[i]]], name = column_names[i], type = 'scatter', mode = 'lines+markers',
-                showlegend = FALSE,
-                visible = if (column_names[i] == selected_column) TRUE else FALSE)
-  }
-  
-  station_name <- unique(df$StationCode)
-  
-  # Customize the layout
-  fig <- fig %>%
-    layout(xaxis = list(title = 'Date'),
-           yaxis = list(title = y_axis_titles[selected_column]),
-           title = list(text = paste0("Mean daily ", selected_column, "<br>for ", loc_name, " (station code: ", station_name, ")"), 
-                        y = 0.90), 
-           margin = list(t = 60),
-           plot_bgcolor = '#e5ecf6',
-           xaxis = list(zerolinecolor = 'black',
-                        zerolinewidth = 2,
-                        gridcolor = 'azure1'),
-           yaxis = list(zerolinecolor = 'black',
-                        zerolinewidth = 2,
-                        gridcolor = 'azure1'))
-  
-  return(fig)
-}
-
+WQ_data_units <- readRDS("./03_Data_for_app/WQ_data_units.Rds")
 
 #### Run the app #### 
 find_directory_of_file("app.R")
@@ -384,8 +242,11 @@ WINPageServer <- function(id, parentSession) {
     })
     
     #### Update plot when dropdown selection and/or date changes ####
-    # Reactive value for the selected column
-    selected_column <- reactive({
+    # Reactive expression that can read reactive values and call other reactive
+    # expressions -> whenever a reactive value changes, the reactive expression will
+    # re-execute
+    # (incorrect explanation?: Reactive value for the selected column)
+    selected_column <- reactive({ # So I can also put this in the function directly??
       print(paste("Selected column changed to:", input$column_selector))
       input$column_selector
     })
@@ -396,14 +257,14 @@ WINPageServer <- function(id, parentSession) {
     })
     
     observeEvent(selected_column(), {
-      req(clicked_station())  # Ensure station is selected
-      #req(date_range())
+      req(clicked_station())  # Ensure station is selected -> if not, nothing happens
+      req(selected_date_range()) # instead of req(), add this to observeEvent also
       
       print(paste("Updating plot for", selected_column(), "at station", clicked_station(), "for", date_range[1], " to ", date_range[2]))
       
       df <- filter_dataframe(WQ_df, filter_value = clicked_station(), date_range = selected_date_range())
       
-      output$plot <- renderPlotly({
+      output$plot <- renderPlotly({ # also add the date range to the if statemenent?
         if (selected_column() %in% names(df)) { # only plot if variable exists for that station
           create_plot(df, units_df = WQ_data_units, loc_name = unique(df$site_friendly), selected_column = selected_column())
         } else {
