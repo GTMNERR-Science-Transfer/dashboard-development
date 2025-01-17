0cb99e1670b6a2641702c913be9cabe51dc712f3
Geraldine Klarenberg <gklarenberg@ufl.edu>
2025-01-17 17:38:49 -0500

    Created files with unique genus and species names, and also separated these in the main HAB file. Created a lookup table indicating the type of algae. Commented out the code that made the HAB data long. Created new HAB.Rds file

--- 02_Cleaning_scripts/HAB_FWC.R
+++ 02_Cleaning_scripts/HAB_FWC.R
@@ -11,19 +11,42 @@
 
 library(tidyverse)
 
-HAB <- read_csv("01_Data_raw/Water_Quality/HAB/HAB_FWC.csv")
+HAB <- read_csv("01_Data_raw/Algae/HAB/HAB_FWC.csv")
 
+# Create a file with all unique species names
+write_csv(data.frame(Species = unique(HAB$Species)), "03_Data_for_app/HAB_species_list.csv")
+
+# Separate genus and species; add column on type based on lookup table
+HAB <- HAB %>% 
+  separate_wider_delim(Species, 
+                       delim = " ", 
+                       names = c("genus"),
+                       too_few = "align_start", # if only one word, make species col NA
+                       too_many = "drop", # if more than 2 words, ignore the extra words
+                       cols_remove = FALSE) 
+
+# Create a file with all unique genus names
+write_csv(data.frame(genus = unique(HAB$genus)), "03_Data_for_app/HAB_genus_list.csv")
+# I (GK) manually added algal types to the genus file (diatoms, cyanobacteria, 
+# dinoflagellates, other)
+# Read in this lookup table
+lookup_genus <- read_csv("03_Data_for_app/HAB_lookup_genus.csv")
+
+HAB <- HAB %>% 
+  left_join(lookup_genus) # Add those types to the main dataset
 
 # Create long format so it can be used in the Shiny app
 # Only doing this with numeric variables for now
-HAB <- HAB %>% 
-  pivot_longer(cols = c("cells/L*", "Temperature (C)", "Salinity", 
-                        "DO (%)", "DO (mg/L)", "pH"),
-               names_to = "vars", values_to = "vals")
-HAB$Date <- dmy(HAB$`Sample Date`)
+# HAB <- HAB %>% 
+#   pivot_longer(cols = c("cells/L*", "Temperature (C)", "Salinity", 
+#                         "DO (%)", "DO (mg/L)", "pH"),
+#                names_to = "vars", values_to = "vals")
+# HAB$Date <- dmy(HAB$`Sample Date`)
 
 # Save it as an .Rds file so it can be read into the Shiny app
 saveRDS(HAB, "03_Data_for_app/HAB.Rds")
+
+###############################################
 
 # Check all unique categories
 unique(HAB$Description)
@@ -46,24 +69,6 @@ ggplot(HAB %>% filter(vars == "Temperature (C)"), aes(x = vals))+
 # NOTE: some only have "present" but no count
 # Make a list of algae that have counts, and those that only have "present"
 
-
-
 algae_present <- HAB %>% filter(Description == "present")
 algae_count <- HAB %>% filter(Description != "present")
-
-algae_time <- ggplot(HAB %>% filter(vars == "cells/L*"), aes(x = Date, y = vals,
-                                               color = Species))+
-  geom_point()+
-  theme(legend.position = "none")
-
-algae_time
-
-
-# Algae counts overall
-
-
-# Algae vs other variables
-ggplot(HAB %>% filter(vars == "cells/L*"))
-
-
 
