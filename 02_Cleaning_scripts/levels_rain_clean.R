cc29230db3547a29ea6ee5a197675a1189545bc5
Geraldine Klarenberg <gklarenberg@ufl.edu>
2025-02-26 09:19:50 -0500

    Started a cleaning script for cleaning water level and rainfall data

---
+++ 02_Cleaning_scripts/levels_rain_clean.R
@@ -0,0 +1,18 @@
+########################################################################
+########## NERRS Science Transfer project - GTMNERR        #############
+########################################################################
+
+# Geraldine Klarenberg, PhD
+# gklarenberg@ufl.edu
+# 25 Feb 2025
+
+# Initial script to clean/visualize rain and water level data, before transferring to a 
+# Shiny script
+
+# Use list files
+# Loop and read in data
+# Pick correct columns (2 rainfall and 2 water level)
+# Probably pivot to make long
+# Add to larger dataset
+# Make separate location file with stations and coordinates
+# But maybe ask Nikki for photos, instead of using a map?
