1a6b6bbed23d77a8414ca16804a0496034873bd9
Geraldine Klarenberg <gklarenberg@ufl.edu>
2025-02-26 19:57:56 -0500

    Created clean precip and dam level datasets as .Rds. Had to make a few small manual changes to the csvs (e.g. remove cols that showed up as NA, remove a separate overview table that was added)

--- 02_Cleaning_scripts/levels_rain_clean.R
+++ 02_Cleaning_scripts/levels_rain_clean.R
@@ -31,12 +31,20 @@ for (fil in precip_files){
   # remove the first two rows, as well as the third row with "Date"
   tst <- tst[-c(1,2,3), ] 
   # Add year as a column
-  tst$year <- str_extract(basename(paste0("01_Data_raw/Hydrology/years/", precip_files[1])), "\\d{4}")
+  tst$year <- str_extract(fil, "\\d{4}")
   # Change the first col name to day
   colnames(tst)[1] <- "day"
   # Remove the last rows, with Totals and YTD, and NAs (at the bottom)
   tst <- tst %>% 
-    filter(day != "Totals", day != "YTD", !is.na(day))
+    filter(day != "Totals", 
+           day != "Total", 
+           day != "TOTAL",
+           day != "YTD", 
+           !is.na(day))
+  
+  # Make sure that "N.Lot" is written without a space
+  
+  
   # Make long so we can start adding everything together
   tst_long <- tst %>% 
     pivot_longer(cols = -c(day, year),
@@ -47,39 +55,58 @@ for (fil in precip_files){
       into = c("month", "location"), 
       sep = "_", 
       remove = TRUE
-    )
+    ) %>% 
+    mutate(location = case_when(location == "N. Lot" ~ "N.Lot",
+                                location == "Dam lvl." ~ "Dam_level",
+                                TRUE ~ location))
   all_data <- bind_rows(all_data, tst_long)
 }
 
-tst = read_csv(paste0("01_Data_raw/Hydrology/years/", precip_files[1]), col_names = FALSE)
-# Fill missing values in the first row (e.g., January, January, February, February)
-tst[1, ] <- t(zoo::na.locf(t(tst[1, ]), na.rm = FALSE))
-# Combine first 2 rows as column names
-colnames(tst) <- paste(tst[1, ], tst[2, ], sep = "_")
-# remove the first two rows, as well as the third row with "Date"
-tst <- tst[-c(1,2,3), ] 
-# Add year as a column
-tst$year <- str_extract(basename(paste0("01_Data_raw/Hydrology/years/", precip_files[1])), "\\d{4}")
-# Change the first col name to day
-colnames(tst)[1] <- "day"
-# Remove the last rows, with Totals and YTD, and NAs (at the bottom)
-tst <- tst %>% 
-  filter(day != "Totals", day != "YTD", !is.na(day))
-# Make long so we can start adding everything together
-tst_long <- tst %>% 
-  pivot_longer(cols = -c(day, year),
-               names_to = "month_location", 
-               values_to = "value") %>%
-  separate(
-    col = month_location, 
-    into = c("month", "location"), 
-    sep = "_", 
-    remove = TRUE
-  )
+# Check
+unique(all_data$day)
+unique(all_data$month)
+unique(all_data$year)
+unique(all_data$value) # contains "trace" and "IRMA". Will be turned into NA
+
+# count NAs
+sum(is.na(all_data$day))
+sum(is.na(all_data$month))
+sum(is.na(all_data$year))
+sum(is.na(all_data$value)) #3836
+
+# Create proper dates
+all_data <- all_data %>% 
+  mutate(year = as.numeric(year),
+         day = as.numeric(day),
+         value = as.numeric(value)) %>% 
+  mutate(date = ymd(paste(year, month, day, sep = "/"))) %>% 
+  filter(!is.na(date))
+# This gives a warning that some dates were unable to parse, but that is fine
+# because those are combinations that don't exist, e.g. 31 November
+
+ggplot(all_data, aes(x = date, y = value, color = location))+
+  geom_point()
+
+precip <- all_data %>% 
+  filter(location == "N.Lot" | location == "Shop")
+
+dam_level <- all_data %>% 
+  filter(location == "Dam_level")
+
+ggplot(precip, aes(x = date, y = value, color = location))+
+  geom_point()
+ggplot(dam_level, aes(x = date, y = value, color = location))+
+  geom_point()
+
+# Check the really high value
+dam_level[which(dam_level$value == max(dam_level$value, na.rm = TRUE)),]
+# 21 March 2023 (value 35)... Outside hurricane season. Suspect it's wrong
+# Checked manually and yes, all month is 3.3, 3.4, 3.5 etc. Replace
+dam_level$value[which(dam_level$value == max(dam_level$value, na.rm = TRUE))] <- 3.5
+ggplot(dam_level, aes(x = date, y = value, color = location))+
+  geom_point()
+
+saveRDS(precip, "03_Data_for_app/precip.Rds")
+saveRDS(dam_level, "03_Data_for_app/dam_level.Rds")
 
 
-# Clean column names (optional, removes spaces and makes names syntactically valid)
-df <- df %>% clean_names()
-
-# View the cleaned data
-print(df)
